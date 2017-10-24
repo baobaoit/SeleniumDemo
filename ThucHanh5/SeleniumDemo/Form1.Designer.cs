93174a2e03f914b2dbfc06bd5f85dc578b7136bd
baobaoit <quocbao.lehoang@gmail.com>
2017-10-24 15:03:38 +0700

    Selenium Demo

---
+++ ThucHanh5/SeleniumDemo/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SeleniumDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(136, 12);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(136, 51);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 125);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ ThucHanh5/SeleniumDemo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo
+{
+    public partial class Form1 : Form
+    {
+        string info;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txtUsername = txtEmail.Text.Trim();
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://dichvu.ou.edu.vn/v2/login.php?ref=aHR0cDovL2RpY2h2dS5vdS5lZHUudm4vdjIvaW5kZXgucGhwP209ZGljaHZ1JmE9eGVtdGti";
+            driver.FindElement(By.Id("txtUsername")).SendKeys(txtUsername);
+            driver.FindElement(By.Id("cmdLogin")).Click();
+
+            info = driver.FindElement(By.CssSelector("#stdinfo > .list-arrow")).Text;
+            driver.Close();
+
+            string[] tachInfo = info.Split('\r', '\n');
+            int viTriDauKhoangTrang = tachInfo[2].IndexOf(':') + 1;
+            string hoTen = tachInfo[2].Substring(viTriDauKhoangTrang + 1);
+
+            MessageBox.Show(hoTen);
+        }
+    }
+}
